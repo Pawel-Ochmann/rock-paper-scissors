24317e5228dfa6865c68376bae852d5a17656a7f
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-26 19:07:27 +0200

    Create crucial functions, adding and removing class which shows players choice. Class related to computers choice still needs fix. Game counts rounds properly, need to display it, and add replay option

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let playerPoints = 0;
 let computerPoints = 0;
+let round = 1;
+
+playerPoints = 0;
+computerPoints = 0;
+
+const choices = document.querySelectorAll('.img');
+choices.forEach((e) => {
+    e.addEventListener('click', getPlayerChoice)
+})
+
+
+const result = document.querySelector("#result");
+const info = document.querySelector(".info>p")
 
 function getComputerChoice(callback) {
     if (callback() === 0) {
@@ -12,67 +25,76 @@ function getComputerChoice(callback) {
 }
 
 function getRandomInt() {
-    return Math.floor(Math.random()*3)
+    return Math.floor(Math.random() * 3)
 }
 
 function getPlayerChoice() {
-    let playerSelection = prompt("Please enter: Rock, Paper or Scissors");
-    return playerSelection.toUpperCase();
+
+    choices.forEach((e) => {
+        e.classList.remove('chosen', 'playerWin', 'playerLose', 'draw')
+    })
+
+    result.textContent = (`Round ${round}!`);
+    this.classList.add('chosen');
+    let playerChoice = this.id;
+    round++;
+
+    return playRound(playerChoice, getComputerChoice(getRandomInt))
 }
 
-function winRound(a,b) {
+function winRound(a, b) {
     playerPoints++;
-    console.log(`You Win! ${a} beats ${b}`)
+    info.textContent = (`You Win! ${a} beats ${b}`)
+    game();
+    document.querySelector(`#${b}`).classList.add("playerWin")
 }
-function loseRound(a,b) {
+function loseRound(a, b) {
     computerPoints++;
-    console.log(`You Lose! ${b} beats ${a}`)
+    info.textContent = (`You Lose! ${b} beats ${a}`)
+    document.querySelector(`#${b}`).classList.add("playerLose")
+    game();
 }
 
-function playRound(playerSelection,computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log(`You both chose ${playerSelection}. Try again!`)
+        info.textContent = (`You both chose ${playerSelection}. Try again!`)
+        document.querySelector(`#${computerSelection}`).classList.add("draw")
     }
     else if (playerSelection === 'ROCK') {
         if (computerSelection === 'PAPER') {
-            return loseRound(playerSelection,computerSelection)
+            return loseRound(playerSelection, computerSelection)
         }
-        else return winRound(playerSelection,computerSelection)
+        else return winRound(playerSelection, computerSelection)
     }
     else if (playerSelection === "PAPER") {
         if (computerSelection === "SCISSORS") {
-            return loseRound(playerSelection,computerSelection)
+            return loseRound(playerSelection, computerSelection)
         }
-        else return winRound(playerSelection,computerSelection)
+        else return winRound(playerSelection, computerSelection)
     }
     else {
-        if (computerSelection=== 'ROCK') {
-            return loseRound(playerSelection,computerSelection)
+        if (computerSelection === 'ROCK') {
+            return loseRound(playerSelection, computerSelection)
         }
-        else return winRound(playerSelection,computerSelection)
+        else return winRound(playerSelection, computerSelection)
     }
 }
 
 
 function game() {
-    playerPoints = 0;
-    computerPoints = 0;
+    console.log(playerPoints, computerPoints)
 
-    for (let i = 1; i<=5; i++) {
-        console.log(`Round ${i}!`)
-        playRound(getPlayerChoice(),getComputerChoice(getRandomInt))
-    }
+    if (playerPoints === 5 || computerPoints === 5) {
 
-    if (computerPoints===playerPoints) {
-        console.log(`It\'s a draw ${playerPoints} : ${computerPoints}`)
+        if (computerPoints === playerPoints) {
+            console.log(`It\'s a draw ${playerPoints} : ${computerPoints}`)
+        }
+        else if (computerPoints > playerPoints) {
+            console.log(`You Lose a Game! ${playerPoints} : ${computerPoints}`)
+        }
+        else console.log(`You Won a Game! ${playerPoints} : ${computerPoints}`)
     }
-    else if (computerPoints>playerPoints) {
-        console.log(`You Lose a Game! ${playerPoints} : ${computerPoints}`)
-    }
-    else console.log(`You Won a Game! ${playerPoints} : ${computerPoints}`)
-
-    const replay = confirm('Do you want to play again?');
-    if (replay) game()
- }
+    else return;
+}
 
 
